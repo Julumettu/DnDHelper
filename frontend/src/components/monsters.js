834fac2b45dddc850235a4e0a7c89766f6f01d5b
julumettu <juhoronkko@hotmail.com>
2020-01-20 19:03:18 +0200

    a bit different way to show monster additional info (Still shows or deletes all of the tables which is a problem)

--- frontend/src/components/monsters.js
+++ frontend/src/components/monsters.js
@@ -4,13 +4,13 @@ import './monsters.css'
 
 const Monsters = ({monsters}) => {
     
-    const [divState, setdivState] = useState("visible")
+    const [divState, setdivState] = useState(false)
     function changeView(){
-	if(divState === "visible"){
-	    setdivState("hidden")
+	if(divState === true){
+	    setdivState(false)
 	}
 	else{
-	    setdivState("visible")
+	    setdivState(true)
 	}
     }
     return (
@@ -24,28 +24,29 @@ const Monsters = ({monsters}) => {
 		<button onClick={() => 
 			changeView()
 		}>Show more/less</button>
-		<table class="more_info" style={{visibility: divState}}>
-		<tr>
-		<td>AC: { monster.armor_class }
-		<br></br>HP: { monster.hit_points } / { monster.hit_points_dice }
-		<br></br>Speed: { monster.speed } ft
-		</td>
-		</tr>
-		<tr>
-		<td>  <b>Ability scores</b> <br></br>
-			<p id="str_mod"> STR { monster.strength } ( </p>
-			<p id="dex_mod"> DEX { monster.dexterity } ( </p>
-			<p id="const_mod"> CONST { monster.constitution } ( </p>
-			<p id="int_mod"> INT { monster.intelligence } ( </p>
-			<p id="wis_mod"> WIS { monster.wisdom } ( </p>
-			<p id="cha_mod"> CHA { monster.charisma } ( </p>
-		</td>
-		</tr>
-		<tr>
-			<td>{ monster.abilities }<br></br>
-		    { monster.actions }</td>
-		</tr>
-	        </table>
+		{ divState &&         <table class="more_info" style={{visibility: divState}}>
+        <tr>
+        <td>AC: { monster.armor_class }
+        <br></br>HP: { monster.hit_points } / { monster.hit_points_dice }
+        <br></br>Speed: { monster.speed } ft
+        </td>
+        </tr>
+        <tr>
+        <td>  <b>Ability scores</b> <br></br>
+                <p id="str_mod"> STR { monster.strength } ( </p>
+                <p id="dex_mod"> DEX { monster.dexterity } ( </p>
+                <p id="const_mod"> CONST { monster.constitution } ( </p>
+                <p id="int_mod"> INT { monster.intelligence } ( </p>
+                <p id="wis_mod"> WIS { monster.wisdom } ( </p>
+                <p id="cha_mod"> CHA { monster.charisma } ( </p>
+        </td>
+        </tr>
+        <tr>
+                <td>{ monster.abilities }<br></br>
+            { monster.actions }</td>
+        </tr>
+        </table>
+}
 		</table>
 		    ))}
 		</div>
