e7b8b14ce928989160814bc8812456751ff75565
julumettu <juhoronkko@hotmail.com>
2020-02-02 14:23:15 +0200

    Added basic search functionality for monster-list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,28 +4,30 @@ import PostFormMonster from './components/post_monster';
 
 
 class App extends Component {
-
    render() {
         return (
 	<div class="all_monsters">
+            <label> Search For Specific monster </label>
+	    <input type="text" id="monster_search" onChange={event => this.state.search_word = event.target.value} ></input>
             <Monsters monsters={this.state.monsters} />
-	    <PostFormMonster />
 	</div>
         )
     }
-
     state = {
         monsters: [],
         search_word : ""
     };
 
-    componentDidMount() {
+    componentDidUpdate() {
         fetch('http://localhost:8000/api/monsters/?search=' + this.state.search_word)
             .then(res => res.json())
             .then((data) => {
                 this.setState({ monsters: data })
             })
             .catch(console.log)
+    }
+    componentDidMount() {
+        this.setState({search_word : this.state.search_word})
     }
 }
 
